6d61f342b7efee591312faff6dbf165d532d0a48
Rehman <rk03229975701@gmail.com>
2023-10-18 23:37:54 -0700

    now create a variables which take input from the User

--- DollarExchange.c
+++ DollarExchange.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 
 int main (){
-    
+
+   const float JPY = 127.65;
+   const float GBP = 0.79; 
+
+   float exchange_rate, USD;
+   printf("pleas input the total amount of US Dollar: ");
+   scanf("%f", &USD);
 }
