7d1bdf2f954b2a2337789eea493f15743f5895b9
Rehman <rk03229975701@gmail.com>
2023-10-18 23:40:42 -0700

    now minus the usd from the exchnage rate for 10% tax

--- DollarExchange.c
+++ DollarExchange.c
@@ -8,4 +8,8 @@ int main (){
    float exchange_rate, USD;
    printf("pleas input the total amount of US Dollar: ");
    scanf("%f", &USD);
+
+   exchange_rate = (USD/10);
+   printf("Exchange_rate = %f\n", exchange_rate);
+   USD = USD - exchange_rate;
 }
