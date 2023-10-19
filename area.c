e5698e3be6e70e466baf9831abd8ea57064f2f34
Rehman <rk03229975701@gmail.com>
2023-10-18 23:05:32 -0700

    Arrange the formula in the corect order

--- area.c
+++ area.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv) {
 
   scanf("%lf", &height);
 
-  area = 1/2 * base * height;
+  area =  base * height * 1/2;
 
   printf("The area is %f square units.\n", area);
 
